5dc3325cfe51f736cd8afba40a18d8e3e9bded81
giotrade81-cyber <giotrade81@gmail.com>
2026-08-26 14:19:18 +0200

    Aggiornamento del sito — 2026-08-26

--- assets/globalVoice-DNmjMw79.js
+++
@@ -1,1 +0,0 @@
-import{V as t}from"./VoiceAssistant-CpTO9j7I.js";document.addEventListener("DOMContentLoaded",()=>{new t("http://localhost:3000/api")});
---
+++ assets/globalVoice-TtsbeCJy.js
@@ -0,0 +1,1 @@
+import{V as t}from"./VoiceAssistant-iA-fUDu8.js";document.addEventListener("DOMContentLoaded",()=>{new t("http://localhost:3000/api")});
